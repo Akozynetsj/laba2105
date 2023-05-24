e0cf7f129322155ef785d75e19c8dc475e74aabd
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-24 16:04:22 +0300

    зоопарк

---
+++ main.py
@@ -0,0 +1,85 @@
+import random
+class Animal:
+    def __init__(self, species, name, age, health, hunger, happiness):
+        self.species = species
+        self.name = name
+        self.age = age
+        self.health = health
+        self.hunger = hunger
+        self.happiness = happiness
+
+    def grow(self):
+        self.age += 1
+        self.health -= random.randint(0, 10)
+        self.hunger += random.randint(0, 10)
+        self.happiness -= random.randint(0, 10)
+
+    def eat(self):
+        if self.hunger >= 10:
+            self.health += random.randint(0, 5)
+            self.hunger -= random.randint(5, 10)
+            self.happiness += random.randint(0, 5)
+        else:
+            print(self.name, "is not hungry")
+
+    def play(self):
+        if self.happiness >= 5:
+            self.health += random.randint(0, 5)
+            self.hunger += random.randint(0, 5)
+            self.happiness += random.randint(0, 5)
+        else:
+            print(self.name, "does not want to play")
+
+    def __str__(self):
+        return f"{self.species} - {self.name} ({self.age} years old)\n" \
+               f"Health: {self.health}\nHunger: {self.hunger}\nHappiness: {self.happiness}\n"
+
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def remove_animal(self, animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+
+    def feed_all(self):
+        for animal in self.animals:
+            animal.eat()
+
+    def play_with(self, animal):
+        if animal in self.animals:
+            animal.play()
+
+    def grow_all(self):
+        for animal in self.animals:
+            animal.grow()
+
+    def __str__(self):
+        return "\n".join([str(animal) for animal in self.animals])
+
+
+def save_zoo_state(zoo, day):
+    filename = f"Day_{day}.txt"
+    with open(filename, "w") as file:
+        file.write(str(zoo))
+
+
+zoo = Zoo()
+zoo.add_animal(Animal("Lion", "Nemo", 8, 60, 1, 3))
+zoo.add_animal(Animal("Giraffe", "fin", 6, 34, 5, 1))
+zoo.add_animal(Animal("Hippo", "sasuke", 5, , 77, 5))
+
+for i in range(1, 11):
+    print(f"\nDay {i}")
+    save_zoo_state(zoo, i)
+    zoo.feed_all()
+    random_animal = random.choice(zoo.animals)
+    zoo.play_with(random_animal)
+    zoo.grow_all()
+    print(zoo)
+
+print("\nSimulation finished")
